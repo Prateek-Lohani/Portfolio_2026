7b422eec73233c9c6fcb04ef8a25189fe79f968f
Prateek-Lohani <prateeklohani105@gmail.com>
2026-01-20 23:12:34 +0530

    added react

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello')
+import React from "react"
+import { createRoot } from 'react-dom/client'
+
+const heading=React.createElement('h1',{},"Portfolio")
+
+const root=createRoot(document.getElementById("root"));
+root.render(heading);
+
+
