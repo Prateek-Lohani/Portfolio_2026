a80588114aa014b45c1721b9312331918ef1c0c3
Prateek-Lohani <prateeklohani105@gmail.com>
2026-01-23 01:54:20 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 const App=()=>{
@@ -16,6 +17,7 @@ const App=()=>{
         <Projects/>
         <Experience/>
         <Contact/>
+        <Footer/>
         </>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer=()=>{
+    return (
+        <section className="w-full p-10 flex items-center justify-end ">
+            Copyright © Prateek Lohani - 2026
+        </section>
+    )
+}
+
+export default Footer;
