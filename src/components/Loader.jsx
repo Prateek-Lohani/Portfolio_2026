af726bf965e6f0b956d2d4aee6871048bda7b3c2
Prateek-Lohani <prateeklohani105@gmail.com>
2026-03-01 13:34:09 +0530

    loader improvements

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,11 @@
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-    <div className="w-20 h-20 border-4 border-t-indigo-500 border-black rounded-full animate-spin"></div>
+    <div className="flex items-center justify-center w-full h-screen relative">
+    <div className="w-20 h-20 border-4 border-t-indigo-500 border-black rounded-full animate-spin">
+    </div>
+    <span className="absolute text-xs top-[58vh]">
+      Loading Assets
+    </span>
     </div>
   );
 };
